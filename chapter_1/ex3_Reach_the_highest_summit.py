422330d369966f9247bffa06bb93e740343d55cc
Mari <marygt182@gmail.com>
2024-04-10 12:11:56 +0100

    Reformat code

--- chapter_1/ex3_Reach_the_highest_summit.py
+++ chapter_1/ex3_Reach_the_highest_summit.py
@@ -1,10 +1,13 @@
 import math
-import random             	# just for generating random mountains                                 	 
+import random  # just for generating random mountains
 
 # generate random mountains                                                                               	 
-w = [random.random()/3, random.random()/3, random.random()/3]
-h = [1.+math.sin(1+x/6.)*w[0]+math.sin(-.3+x/9.)*w[1]+math.sin(-.2+x/30.)*w[2] for x in range(100)]
-h[0] = 0.0; h[99] = 0.0
+w = [random.random() / 3, random.random() / 3, random.random() / 3]
+h = [1. + math.sin(1 + x / 6.) * w[0] + math.sin(-.3 + x / 9.) * w[1] + math.sin(-.2 + x / 30.) * w[2] for x in
+     range(100)]
+h[0] = 0.0;
+h[99] = 0.0
+
 
 def climb(x, h):
     # keep climbing until we've found a summit
@@ -12,10 +15,10 @@ def climb(x, h):
 
     # edit here
     while not summit:
-        summit = True         # stop unless there's a way up
+        summit = True  # stop unless there's a way up
         if h[x + 1] > h[x]:
-            x = x + 1         # right is higher, go there
-            summit = False    # and keep going
+            x = x + 1  # right is higher, go there
+            summit = False  # and keep going
         elif h[x - 1] > h[x]:
             x = x - 1
             summit = False
@@ -23,12 +26,12 @@ def climb(x, h):
 
 
 def main(h):
-
-    # start at a random place                                                                                  	 
+    # start at a random place
     x0 = random.randint(1, 98)
     x = climb(x0, h)
 
     print("Venla started at %d and got to %d" % (x0, x))
     return x0, x
+
 
 main(h)

6936595597ae5277880af6eaaac0f96c1acbd60e
MariGsp <marilena.gaspari@viabledata.co.uk>
2024-07-23 11:29:33 +0100

    Add ex. 13

---
+++ chapter_3/ex13_predicitons_with_more_data.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# Try modifying the code below by adding the numbers for a sixth cabin into the data
+# so that you have the following data set
+# size, size of the sauna, distance to water, indoor bathrooms, proximity of neighbors,price in € (output)
+
+# cabin 1	25	2	50	1	500	    127,900
+# cabin 2	39	3	10	1	1000	222,100
+# cabin 3	13	2	13	1	1000	143,750
+# cabin 4	82	5	20	2	120	    268,000
+# cabin 5	130	6	10	2	600	    460,700
+# cabin 6	115	6	10	1	550	    407,000
+
+def main():
+    np.set_printoptions(precision=1)
+
+    x = np.array(
+        [
+            [25, 2, 50, 1, 500],
+            [39, 3, 10, 1, 1000],
+            [13, 2, 13, 1, 1000],
+            [82, 5, 20, 2, 120],
+            [130, 6, 10, 2, 600],
+            [115, 6, 10, 1, 550]
+        ]
+    )
+
+    y = np.array([127900, 222100, 143750, 268000, 460700, 407000])
+
+    c = np.linalg.lstsq(x, y)[0]
+    print(c)
+
+    print(x @ c)
+
+main()
